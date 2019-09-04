2966309d2cbb799804976724df4b0a8419d4472a
odiwml147 <odiwml147@163.com>
2019-09-04 11:35:31 +0800

    初始化提交

--- src/org/test/study/factorypattern/FactoryPatternDemo.java
+++ src/org/test/study/factorypattern/FactoryPatternDemo.java
@@ -2,8 +2,6 @@ package org.test.study.factorypattern;
 
 public class FactoryPatternDemo {
  
-	
-	
    public static void main(String[] args) {
       ShapeFactory shapeFactory = new ShapeFactory();
  
